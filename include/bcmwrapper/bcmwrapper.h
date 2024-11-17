0e9493cb2023d5cd149d7b1b86479d22b725b83a
GioByte10 <gvanni.bernal10@gmail.com>
2024-11-17 04:49:06 -0800

    Added more functions to bcmwrapper

--- include/bcmwrapper/bcmwrapper.h
+++ include/bcmwrapper/bcmwrapper.h
@@ -5,12 +5,25 @@
 #include <cstdint>
 #include <sys/types.h>
 
-inline void gpio_pinset(uint8_t pin, uint8_t mode){
+constexpr uint8_t OUTPUT	= BCM2835_GPIO_FSEL_OUTP;
+constexpr uint8_t INPUT		= BCM2835_GPIO_FSEL_INPT;
+constexpr uint8_t PULLUP	= BCM2835_GPIO_PUD_UP;
+constexpr uint8_t PULLDOWN	= BCM2835_GPIO_PUD_DOWN;
+
+inline void gpio_pinmode(uint8_t pin, uint8_t mode){
 	bcm2835_gpio_fsel(pin, mode);
 }
 
 inline void gpio_write(uint8_t pin, uint8_t value){
 	bcm2835_gpio_write(pin, value);
+}
+
+inline uint8_t gpio_read(uint8_t pin){
+	return bcm2835_gpio_lev(pin);
+}
+
+inline void gpio_setpull(uint8_t pin, uint8_t pull){
+	bcm2835_gpio_set_pud(pin, pull);
 }
 
 
--- src/blink.cpp
+++ src/blink.cpp
@@ -1,19 +1,33 @@
 #include "../include/bcmwrapper/bcmwrapper.h"
 #include <stdio.h>
+#include <signal.h>
 
 #define LED_PIN RPI_GPIO_P1_11
 
+void handle_signal(int signal){
+	printf("\nTerminating from signal %d\n", signal);
+
+	bcm2835_close();
+	printf("bcm2835 closed\n");
+
+	exit(0);
+}
+
 int main() {
+
+	signal(SIGINT, handle_signal);
+	signal(SIGTERM, handle_signal);
     
+		
     if (!bcm2835_init()) {
         printf("bcm2835 initialization failed. Are you running as root?\n");
         return 1;
     }
 
-    gpio_pinset(LED_PIN, BCM2835_GPIO_FSEL_OUTP);
+    gpio_pinmode(LED_PIN, BCM2835_GPIO_FSEL_OUTP);
 
     while (1) {
-		printf("blinking...\n");
+	//	printf("blinking...\n");
         gpio_write(LED_PIN, HIGH);
         bcm2835_delay(500);
 
